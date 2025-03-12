efb6f0b69090a873a0e0980a31bfa04ee6176bbd
Ranjith K <thisisranjithk@gmail.com>
2025-03-13 01:29:23 +0530

    integ: Genre list

--- src/components/Genre/GenreList.tsx
+++ src/components/Genre/GenreList.tsx
@@ -1,14 +1,27 @@
 import useGeners from "@/hooks/useGeners";
+import { getCroppedImageUrl } from "@/services/image-url";
+import { Flex, Image, List, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
   const { data } = useGeners();
 
   return (
-    <ul>
+    <List.Root listStyle="none">
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <List.Item key={genre.id} mb={3}>
+          <Flex gapX={3}>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              alt={genre.name}
+              boxSize="32px"
+              objectFit="cover"
+              borderRadius="5px"
+            ></Image>
+            <Text>{genre.name}</Text>
+          </Flex>
+        </List.Item>
       ))}
-    </ul>
+    </List.Root>
   );
 };
 
