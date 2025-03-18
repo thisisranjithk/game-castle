ae1ae8fb448628bc60a9f9b9de6a7812aae60430
Ranjith K <thisisranjithk@gmail.com>
2025-03-19 00:57:52 +0530

    optimize: use-debound integ to search input

--- src/components/common/SearchInput.tsx
+++ src/components/common/SearchInput.tsx
@@ -1,18 +1,28 @@
 import { Input, Kbd } from "@chakra-ui/react";
 import { InputGroup } from "../ui/input-group";
 import { LuSearch } from "react-icons/lu";
+import { useDebouncedCallback } from "use-debounce";
 
 interface Props {
   setSearchValue: (value: string) => void;
 }
 
 const SearchInput = ({ setSearchValue }: Props) => {
+  const debounced = useDebouncedCallback(
+    // function
+    (value) => {
+      setSearchValue(value);
+    },
+    // delay in ms
+    800
+  );
+
   return (
     <InputGroup flex="1" startElement={<LuSearch />} endElement={<Kbd>⌘K</Kbd>}>
       <Input
         placeholder="Search games..."
         type="text"
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={(e) => debounced(e.target.value)}
       />
     </InputGroup>
   );
