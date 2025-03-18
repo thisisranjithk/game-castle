4a18aa3d9dd7e3209d0d52649afc8ce40054ce05
Ranjith K <thisisranjithk@gmail.com>
2025-03-18 23:58:44 +0530

    refactor: React-query to fetch genres

--- src/hooks/useGeners.tsx
+++ src/hooks/useGeners.tsx
@@ -1,10 +1,20 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "@/services/api-client";
+import { FetchResponse } from "@/utils/interfaces";
+import { genres } from "@/utils/constants";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGeners = () => useData<Genre>("/genres");
+const useGeners = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default useGeners;
